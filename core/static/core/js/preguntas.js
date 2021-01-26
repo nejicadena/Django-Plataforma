05f93f0e2c2ad6b7cf670a452350cb2d1e456b70
nejicadena <nejicadena@gmail.com>
2021-01-26 11:21:26 -0600

    new templates.02

---
+++ core/static/core/js/preguntas.js
@@ -0,0 +1,17 @@
+function funtion() {
+  var x = document.getElementById("si");
+  if (x.style.display === "none" || x.style.display === '') {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function funtion1() {
+  var x = document.getElementById("no");
+  if (x.style.display === "none" || x.style.display === '') {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
