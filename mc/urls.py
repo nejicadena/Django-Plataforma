24b467aa59db106c6c8c1e00a336c30a835aabab
nejicadena <nejicadena@gmail.com>
2021-02-01 13:07:33 -0600

    fix mc

--- mc/urls.py
+++ mc/urls.py
@@ -4,13 +4,13 @@ from .views import colePageView, cole1PageView, cole2PageView, cole3PageView, co
 urlpatterns = [
     path('colectivo/', colePageView.as_view(), name="colectivo"),
     path('colectivo_1/', cole1PageView.as_view(), name="colectivo_1"),
-    path('colectivo_1/', cole2PageView.as_view(), name="colectivo_2"),
-    path('colectivo_1/', cole3PageView.as_view(), name="colectivo_3"),
-    path('colectivo_1/', cole4PageView.as_view(), name="colectivo_4"),
-    path('colectivo_1/', cole5PageView.as_view(), name="colectivo_5"),
-    path('colectivo_1/', cole6PageView.as_view(), name="colectivo_6"),
-    path('colectivo_1/', cole8PageView.as_view(), name="colectivo_7"),
-    path('colectivo_1/', coleFinalPageView.as_view(), name="colectivo_final"),
+    path('colectivo_2/', cole2PageView.as_view(), name="colectivo_2"),
+    path('colectivo_3/', cole3PageView.as_view(), name="colectivo_3"),
+    path('colectivo_4/', cole4PageView.as_view(), name="colectivo_4"),
+    path('colectivo_5/', cole5PageView.as_view(), name="colectivo_5"),
+    path('colectivo_6/', cole6PageView.as_view(), name="colectivo_6"),
+    path('colectivo_8/', cole8PageView.as_view(), name="colectivo_8"),
+    path('colectivo_final/', coleFinalPageView.as_view(), name="colectivo_final"),
     
 
 ]
