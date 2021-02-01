3ae5bc11409794f333aa66fb1c6f09c0e4152634
nejicadena <nejicadena@gmail.com>
2021-02-01 12:43:26 -0600

    add paths mc

--- mc/urls.py
+++ mc/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import colePageView
+from .views import colePageView, cole1PageView, cole2PageView, cole3PageView, cole4PageView, cole5PageView, cole6PageView, cole6PageView, cole8PageView, coleFinalPageView
 
 urlpatterns = [
     path('colectivo/', colePageView.as_view(), name="colectivo"),
-    # path('mc/', MCPageView.as_view(), name="mc")
+    path('colectivo_1/', cole1PageView.as_view(), name="colectivo_1"),
+    path('colectivo_1/', cole2PageView.as_view(), name="colectivo_2"),
+    path('colectivo_1/', cole3PageView.as_view(), name="colectivo_3"),
+    path('colectivo_1/', cole4PageView.as_view(), name="colectivo_4"),
+    path('colectivo_1/', cole5PageView.as_view(), name="colectivo_5"),
+    path('colectivo_1/', cole6PageView.as_view(), name="colectivo_6"),
+    path('colectivo_1/', cole8PageView.as_view(), name="colectivo_7"),
+    path('colectivo_1/', coleFinalPageView.as_view(), name="colectivo_final"),
+    
+
 ]
--- mc/views.py
+++ mc/views.py
@@ -5,3 +5,28 @@ from django.views.generic.base import TemplateView
 
 class colePageView(TemplateView):
     template_name = 'mc/colectivo.html'
+
+class cole1PageView(TemplateView):
+    template_name = 'mc/colectivo_1.html'
+
+class cole2PageView(TemplateView):
+    template_name = 'mc/colectivo_2.html'
+
+class cole3PageView(TemplateView):
+    template_name = 'mc/colectivo_3.html'
+
+class cole4PageView(TemplateView):
+    template_name = 'mc/colectivo_4.html'
+
+class cole5PageView(TemplateView):
+    template_name = 'mc/colectivo_5.html'
+
+class cole6PageView(TemplateView):
+    template_name = 'mc/colectivo_6.html'
+
+class cole8PageView(TemplateView):
+    template_name = 'mc/colectivo_8.html'    
+
+class coleFinalPageView(TemplateView):
+    template_name = 'mc/colectivo_final.html'  
+
